2c0171fc149bcc2c27bc1d9a8725f2cab8329740
Alex Nibley <alexnibley@gmail.com>
2017-04-19 00:52:16 -0400

    App now runs auth tutorial

---
+++ config/auth.js
@@ -0,0 +1,26 @@
+// config/auth.js
+
+// expose our config directly to our application using module.exports
+module.exports = {
+
+    'facebookAuth' : {
+        'clientID'        : 'your-secret-clientID-here', // your App ID
+        'clientSecret'    : 'your-client-secret-here', // your App Secret
+        'callbackURL'     : 'http://localhost:8080/auth/facebook/callback',
+        'profileURL': 'https://graph.facebook.com/v2.5/me?fields=first_name,last_name,email'
+
+    },
+
+    'twitterAuth' : {
+        'consumerKey'        : 'your-consumer-key-here',
+        'consumerSecret'     : 'your-client-secret-here',
+        'callbackURL'        : 'http://localhost:8080/auth/twitter/callback'
+    },
+
+    'googleAuth' : {
+        'clientID'         : '1006083435434-j4n89qob4kjrfm6a9r7u3jtp02npl7qu.apps.googleusercontent.com',
+        'clientSecret'     : '1kcJl8X_p-Er9acD6wf0bjpb',
+        'callbackURL'      : 'http://localhost:8080/auth/google/callback'
+    }
+
+};
--- server.js
+++ server.js
@@ -1,23 +1,47 @@
-var express = require('express');
-    // path = require('path'),
-    // bodyParser = require('body-parser'),
-    // anyDB = require('any-db');
+// server.js
 
-var app = express();
-app.set('view engine', 'hjs');
-app.set('view options', {layouts: true});
-app.set('views', __dirname + '/views');
+// set up ======================================================================
+// get all the tools we need
+var express  = require('express');
+var app      = express();
+var port     = process.env.PORT || 8080;
+var mongoose = require('mongoose');
+var passport = require('passport');
+var flash    = require('connect-flash');
 
-// app.use(bodyParser.urlencoded({extended:false}));
-// app.use(bodyParser.json());
+var morgan       = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser   = require('body-parser');
+var session      = require('express-session');
 
-// ****************************************************************************** //
-// ****************************************************************************** //
+var configDB = require('./config/database.js');
 
-// Static files
-// app.use('/static',express.static(path.join(__dirname, '/static')));
+// configuration ===============================================================
+mongoose.connect(configDB.url); // connect to our database
 
-// Start server
-app.listen(8080, function() {
-    console.log('- Server listening on port 8080');
-});
+require('./config/passport')(passport); // pass passport for configuration
+
+// set up our express application
+app.use(morgan('dev')); // log every request to the console
+app.use(cookieParser()); // read cookies (needed for auth)
+app.use(bodyParser.json()); // get information from html forms
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.set('view engine', 'ejs'); // set up ejs for templating
+
+// required for passport
+app.use(session({
+    secret: 'ilovescotchscotchyscotchscotch', // session secret
+    resave: true,
+    saveUninitialized: true
+}));
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
+
+// routes ======================================================================
+require('./app/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
+
+// launch ======================================================================
+app.listen(port);
+console.log('Server listening on ' + port);
