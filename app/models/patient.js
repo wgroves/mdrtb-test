966197daef2f62ca067a8a195c1df615944d1446
William Groves <grover@WillG.local>
2017-04-21 18:28:23 -0400

    patient Schema

--- app/models/patient.js
+++ app/models/patient.js
@@ -4,7 +4,12 @@ var bcrypt   = require('bcrypt-nodejs');
 
 // define the schema for our patient model
 var patientSchema = mongoose.Schema({
-
+	last_name: String,
+	first_name: String,
+	middle_name: String,
+	dob: Date,
+	sex: String,
+	date_entered: Date
 });
 
 // create the model for users and expose it to our app
